e92586a4bb7348a7746c649fcca6503bd74f76b2
Raptor <github@0raptor.earth>
2023-07-23 14:10:14 +0200

    Projektdateien hinzufügen.

---
+++ CineBank/Views/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CineBank
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // generate demo data
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie() { 
+                Title = "Test 1",
+                Description = "saohf sdhUIBfuzsdgbfuzsdfgb udfgusgfs fas",
+                CoverPath = @"E:\Bilder\IFS-Ahorn.PNG",
+                Genre = "Horror",
+                Duration = "2:34:56",
+                Type = "Movie"
+            });
+            movies.Add(new Movie()
+            {
+                Title = "Test 2",
+                Description = "asdh iAhsdui sdaisgOIG SAuifi agsfgis fg",
+                CoverPath = @"E:\Bilder\20190927_135003000_iOS.jpg",
+                Genre = "Dokumentation",
+                Duration = "21",
+                Type = "Series"
+            });
+            lbMovies.ItemsSource = movies;
+        }
+
+        private void lbMovies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // check if selection is valid
+            if (lbMovies.SelectedItem == null)
+                return;
+            // extract selected object
+            Movie mov = lbMovies.SelectedItem as Movie;
+            // display object in priview
+            imgCoverPath.Source = new BitmapImage(new Uri(mov.CoverPath));
+            tbTitle.Text = mov.Title;
+            tbDescription.Text = mov.Description;
+            tbType.Text = mov.Type;
+            tbGenre.Text = mov.Genre;
+            tbDuration.Text = mov.Duration;
+        }
+
+        #region MenuItem
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# CineBank
+
+Open source local video management solution for Windows. Manage all your movies and series inside a central tool, store metadata and launch them in your prefered media player.
+
+Third implementation of its kind. Last try: [LocalCineBank](https://git.0raptor.earth/Raptor/LocalCineBank) (never published)
+
+## Installation
+
+## Initial configuration
+
+## License
+
+This application is published undes GNU GNU GENERAL PUBLIC LICENSE Version 3 as refered in the LICENSE-File.
+
+Copyright (C) 2023 0Raptor
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see [www.gnu.org/licenses/](https://www.gnu.org/licenses/).
